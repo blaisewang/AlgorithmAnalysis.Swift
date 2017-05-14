9e3d6d8a083a9f96cc7525580777e529993fbe91
blaisewang <kaito.kaito.k@gmail.com>
2017-05-14 23:40:25 +0800

    code refactoring and add paths

--- AlgorithmAnalysis.Swift/Dijkstra.playground/Contents.swift
+++ AlgorithmAnalysis.Swift/Dijkstra.playground/Contents.swift
@@ -7,9 +7,10 @@ typealias edge = (Character, Character)
 typealias weightedEdge = (edge, UInt)
 
 extension Collection where Iterator.Element == weightedEdge, SubSequence.Iterator.Element == Iterator.Element {
-    func shortestPaths(sourcea source: Character) -> [weightedEdge] {
+    func shortestPaths(sourceNode source: Character) -> [([Character], UInt)] {
         var distanceDict = Dictionary<Character, UInt>()
-        var shortestDistance = [weightedEdge]()
+        var previousDict = Dictionary<Character, Character>()
+        var shortestDistance = [([Character], UInt)]()
         forEach {
             distanceDict.updateValue(UInt.max, forKey: $0.0.0)
             distanceDict.updateValue(UInt.max, forKey: $0.0.1)
@@ -20,25 +21,31 @@ extension Collection where Iterator.Element == weightedEdge, SubSequence.Iterato
             guard let minDistance = distanceDict.min(by: { $0.0.value < $0.1.value }) else {
                 break
             }
-            forEach { anEdge in
-                if anEdge.0.0 == minDistance.key {
-                    if let neighborDistance = distanceDict[anEdge.0.1]  {
-                        let alt = anEdge.1 + minDistance.value
+            forEach { weightedEdge in
+                if weightedEdge.0.0 == minDistance.key {
+                    if let neighborDistance = distanceDict[weightedEdge.0.1] {
+                        let alt = weightedEdge.1 + minDistance.value
                         if alt < neighborDistance {
-                            distanceDict.updateValue(alt, forKey: anEdge.0.1)
+                            distanceDict.updateValue(alt, forKey: weightedEdge.0.1)
+                            previousDict.updateValue(weightedEdge.0.0, forKey: weightedEdge.0.1)
                         }
                     }
- 
                 }
             }
-            shortestDistance.append(((source, minDistance.key), minDistance.value))
+            
+            var path: [Character] = [source]
+            var node = minDistance.key
+            while let previousNode = previousDict[node] {
+                path.insert(node, at: 1)
+                node = previousNode
+            }
+            shortestDistance.append(([], minDistance.value))
             distanceDict.removeValue(forKey: minDistance.key)
         }
         
         return shortestDistance
     }
 }
-
 
 var graph: [weightedEdge] = [(("s", "t"), 10),
                              (("s", "y"), 5),
@@ -50,7 +57,6 @@ var graph: [weightedEdge] = [(("s", "t"), 10),
                              (("y", "z"), 2),
                              (("z", "s"), 7),
                              (("z", "x"), 6)]
-print(graph.shortestPaths(sourcea: "s"))
-
+print(graph.shortestPaths(sourceNode: "s"))
 
 
