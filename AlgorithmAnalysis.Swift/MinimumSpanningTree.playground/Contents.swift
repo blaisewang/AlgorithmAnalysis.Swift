144ee5696d60eb8ca42ab0098bb6683305362f2a
blaisewang <kaito.kaito.k@gmail.com>
2017-05-17 18:12:01 +0800

    add maxWeight function

--- AlgorithmAnalysis.Swift/MinimumSpanningTree.playground/Contents.swift
+++ AlgorithmAnalysis.Swift/MinimumSpanningTree.playground/Contents.swift
@@ -39,35 +39,46 @@ extension Collection where Iterator.Element == weightedEdge, SubSequence.Iterato
         return minimumSpanningTree
     }
     
+    var maxWeight: UInt {
+        guard let max = self.max(by: { first, second -> Bool in
+            return first.1 < second.1
+        }) else {
+            return 0
+        }
+        return max.1
+    }
+    
     var totalWeight: UInt {
         return reduce(0, { $0 + $1.1 })
     }
 }
 
-var graph: [weightedEdge] = [((1, 2), 4),
-                             ((2, 3), 8),
-                             ((3, 4), 7),
-                             ((3, 6), 4),
-                             ((4, 5), 9),
-                             ((6, 7), 2),
-                             ((7, 8), 1),
-                             ((8, 9), 7),
-                             ((8, 1), 8),
-                             ((9, 3), 2),
-                             ((9, 7), 6),
-                             ((5, 6), 10),
-                             ((8, 2), 11),
-                             ((4, 6), 14)]
-//var graph: [weightedEdge] = [((1, 2), 28),
-//                             ((1, 6), 10),
-//                             ((2, 7), 14),
-//                             ((2, 3), 16),
-//                             ((6, 5), 25),
-//                             ((7, 5), 24),
-//                             ((7, 4), 18),
-//                             ((3, 4), 12),
-//                             ((5, 4), 22)]
+//var graph: [weightedEdge] = [((1, 2), 4),
+//                             ((2, 3), 8),
+//                             ((3, 4), 7),
+//                             ((3, 6), 4),
+//                             ((4, 5), 9),
+//                             ((6, 7), 2),
+//                             ((7, 8), 1),
+//                             ((8, 9), 7),
+//                             ((8, 1), 8),
+//                             ((9, 3), 2),
+//                             ((9, 7), 6),
+//                             ((5, 6), 10),
+//                             ((8, 2), 11),
+//                             ((4, 6), 14)]
+var graph: [weightedEdge] = [((1, 2), 28),
+                             ((1, 6), 10),
+                             ((2, 7), 14),
+                             ((2, 3), 16),
+                             ((6, 5), 25),
+                             ((7, 5), 24),
+                             ((7, 4), 18),
+                             ((3, 4), 12),
+                             ((5, 4), 22)]
 var mst = graph.minimumSpanningTree
 print(mst)
+print(mst.maxWeight)
 print(mst.totalWeight)
+
 
